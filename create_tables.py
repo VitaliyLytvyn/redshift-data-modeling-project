3892783038f39ea767dead3f00cdf85513c48b57
Vitaliy Lytvyn <vitaliy.lytvyn@swanteams.com>
2021-08-17 15:30:31 +0300

    add docstrings and comments

--- create_tables.py
+++ create_tables.py
@@ -4,24 +4,37 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Drops all the tables in Redshift defined in sql_queries.py
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates tabless in Redshift defined in sql_queries.py
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Entry point.
+    """
+    
+    # Read the configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Create the connection to a running Redshift cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
+    # Delete, if exist, and create anew tables in the Redshift cluster
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
--- etl.py
+++ etl.py
@@ -4,24 +4,39 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load data from S3 to staging tables on Redshift.
+    """
+
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Load data from staging tables to analytics tables on Redshift.
+    """
+
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Entry point.
+    """
+
+    # Read the configuration
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
+    # Create the connection to a running Redshift cluster
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
+    # Load data to Redshift cluster
     load_staging_tables(cur, conn)
     insert_tables(cur, conn)
 
